b8d0b9bbd39624c85a52e147d16db702664d7a30
tomocrafter <tomoppeko@gmail.com>
2020-05-06 21:22:27 +0900

    Added profile update endpoint

--- twitter/accounts.go
+++ twitter/accounts.go
@@ -35,3 +35,22 @@ func (s *AccountService) VerifyCredentials(params *AccountVerifyParams) (*User, 
 	resp, err := s.sling.New().Get("verify_credentials.json").QueryStruct(params).Receive(user, apiError)
 	return user, resp, relevantError(err, *apiError)
 }
+
+type AccountProfileParams struct {
+	Name             string `url:"name,omitempty"`
+	URL              string `url:"url,omitempty"`
+	Location         string `url:"location,omitempty"`
+	Description      string `url:"description,omitempty"`
+	ProfileLinkColor string `url:"profile_link_color,omitempty"`
+	IncludeEntities  *bool  `url:"include_entities,omitempty"`
+	SkipStatus       *bool  `url:"skip_status,omitempty"`
+}
+
+// UpdateProfile modifies the user profile. and returns modified user
+// Requires a user auth context.
+func (s *AccountService) UpdateProfile(params *AccountProfileParams) (*User, *http.Response, error) {
+	user := new(User)
+	apiError := new(APIError)
+	resp, err := s.sling.New().Post("update_profile.json").QueryStruct(params).Receive(user, apiError)
+	return user, resp, relevantError(err, *apiError)
+}
